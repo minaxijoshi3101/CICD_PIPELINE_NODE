12243f9367aefacf91c58171a299ca22bca09551
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-01-31 15:27:49 +0530

    Update checkoutSCM.groovy

--- src/com/poc/util/checkoutSCM.groovy
+++ src/com/poc/util/checkoutSCM.groovy
@@ -3,6 +3,7 @@ def call(Map pipelineParams)
 {
     
     SCM_URL='git@github.com:'+pipelineParams.GIT_GROUP+'/'+pipelineParams.REPO+ '.git';
+    echo "Code checkout from SCM Repo ${SCM_URL}"
     sh '''
     rm -rf ${REPO}
     git clone --single-branch --branch ${BRANCH} ${SCM_URL}
