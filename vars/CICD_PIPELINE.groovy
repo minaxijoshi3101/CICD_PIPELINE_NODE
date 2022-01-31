47abed2e7fc89b566cf641561f3a8882e6da89b4
minaxi joshi <minaxijoshi3101@gmail.com>
2022-01-31 15:06:30 +0530

    checkout code added

--- vars/CICD_PIPELINE.groovy
+++ vars/CICD_PIPELINE.groovy
@@ -1,12 +1,16 @@
-def call()
+import com.poc.util.checkoutSCM;
+
+def call(Map pipelineParams)
 {
+    env.GIT_GROUP = pipelineParams.GIT_GROUP
+    env.REPO = pipelineParams.REPO
     pipeline
     {
         node("master")
         {
             stage("code checkout")
             {
-                
+                new checkoutSCM().call(pipelineParams);
             }
         }
     }
